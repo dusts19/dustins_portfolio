225c0aba06ed39ab39378cc725cfaaf2ad1cf0b3
Dustin Shin <dustin.j.shin@gmail.com>
2025-06-25 02:42:52 -0700

    updated project task manager pic; fixed dark mode contact page inputs

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -11,7 +11,7 @@ const Input: React.FC<InputProps> = ({ label,  name, type="text", required=false
         <div className="flex flex-col">
             <label htmlFor={name}>{label}</label>
             <input
-                className="border-b-2 border-gray-600 w-80 pl-2 bg-gray-100 dark:placeholder:text-slate-800"
+                className="border-b-2 border-gray-600 w-80 pl-2 bg-gray-100 dark:placeholder:text-slate-800 dark:text-slate-800"
                 type={type}
                 id={name}
                 name={name}
